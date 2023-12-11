ab1ff39333376f600e67e3c11abf96e079076111
AnPhu-D <anphudung03@gmail.com>
2023-12-10 22:26:30 -0800

    Add jank coloring

--- src/algorithm/KruskalMST.js
+++ src/algorithm/KruskalMST.js
@@ -1,3 +1,4 @@
+import { useId } from 'react';
 import Graph from '../util/Graph';
 import BaseAlgorithm from './BaseAlgorithm';
 
@@ -44,44 +45,89 @@ class KruskalMST extends BaseAlgorithm {
     // --- process edgelist
     // something something .from.sort.foreach to clean up this later
     let sortedEdgeIndexes = [];
-    for (let m = 0; m < graphInput.edges.length; ++m) {
-      sortedEdgeIndexes.push(m);
-    }
-    sortedEdgeIndexes.sort(function (edge1, edge2){ // Sort in ascending order by edge weight
-      return graphInput.edges[edge1][2] - graphInput.edges[edge2][2];
+    graph.subroutine('sort edges', () => {
+      for (let m = 0; m < graphInput.edges.length; ++m) {
+        sortedEdgeIndexes.push(m);
+      }
+      sortedEdgeIndexes.sort(function (edge1, edge2){ // Sort in ascending order by edge weight
+        return graphInput.edges[edge1][2] - graphInput.edges[edge2][2];
+      });
+      for (let m = 0; m < graphInput.edges.length; ++m) {
+        sortedEdgesTable.set(0, m, sortedEdgeIndexes[m]);
+      }
     });
-    for (let m = 0; m < graphInput.edges.length; ++m) {
-      sortedEdgesTable.set(0, m, sortedEdgeIndexes[m]);
-    }
 
     // --- process graph
     let dsu = new DisjointSetUnion();
     let numEdges = 0;
 
-    for (let i = 0; i < graphInput.n; ++i) {
-      dsu.make_set(i);
-      const u = graph.getVertex(i);
-    }
+    graph.subroutine('color initial disjoint trees', () => {
+      for (let i = 0; i < graphInput.n; ++i) {
+        dsu.make_set(i);
+        const u = graph.getVertex(i);
+        
+        // Assign each vertex a color
+        const ucolor = dsu.find_set(i);
+        u.setColor(ucolor);
+      }
+    });
+
+    let usedEdges = [];
 
     // Consider edges in order of ascending weight
     for (const edgeIndex of sortedEdgeIndexes) { // [from, to, weight]
       // Terminate once MST has been constructed
-      if (numEdges >= graphInput.n - 1) {
-        break;
-      }
-
       const edge = graphInput.edges[edgeIndex];
       const from = edge[0];
       const to = edge[1];
       const weight = edge[2];
 
+      const uv = graph.getEdge(from,to);
+
       const fromSet = dsu.find_set(from);
       const toSet = dsu.find_set(to);
       if (fromSet != toSet) {
         dsu.union_sets(fromSet, toSet);
+        const queryParent = dsu.find_set(from);
+        
+        usedEdges.push(edgeIndex);
+        uv.setColor(queryParent);
+
+        graph.subroutine('update tree colors', () => {
+
+          for (let i = 0; i < graphInput.n; ++i) {
+            if (dsu.find_set(i) == queryParent) {
+              const treeNode = graph.getVertex(i);
+              treeNode.setColor(queryParent);
+            }
+          }
+
+        });
+
         numEdges += 1;
+      } else {
+        uv.setColor(0);
       }
     }
+
+    graph.subroutine('final coloring', () => {
+      for (const edge of graphInput.edges) {
+        const uv = graph.getEdge(edge[0], edge[1]);
+        uv.setColor(0);
+      }
+      for (let i = 0; i < graphInput.n; ++i) {
+        const node = graph.getVertex(i);
+        node.setColor(2);
+      }
+      for (const edgeIndex of usedEdges) {
+        const edge = graphInput.edges[edgeIndex];
+        const uv = graph.getEdge(edge[0], edge[1]);
+        uv.setColor(2);
+
+        const vu = graph.getEdge(edge[1], edge[0]);
+        vu.setColor(2);
+      }
+    });
 
     return graph;
   }
