1df629fa2bb8f4fe16663e7b516b66a4ad58fcc7
NovaSagittarii <novasagittarii@gmail.com>
2023-12-10 17:57:27 -0700

    algorithm template

---
+++ src/algorithm/DFS.js
@@ -0,0 +1,15 @@
+import Graph from '../util/Graph';
+import BaseAlgorithm from './BaseAlgorithm';
+
+class DFS extends BaseAlgorithm {
+  /** @override */
+  run(graphInput) {
+    // --- set up auxiliary values then finalize
+    const graph = new Graph(graphInput, null, null);
+    graph.finalize();
+
+    // --- process graph
+    return graph;
+  }
+}
+export default DFS;
