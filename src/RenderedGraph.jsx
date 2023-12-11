5722b4cc56aead2a558c161981f2dd41ee616a51
Joshua <joshuahsu108@gmail.com>
2023-12-11 00:44:28 -0800

    Add voronoi and draggable

--- src/RenderedGraph.jsx
+++ src/RenderedGraph.jsx
@@ -3,11 +3,14 @@ import { ReactP5Wrapper } from '@p5-wrapper/react';
 import Vector2 from './util/Vector2';
 import GraphPlayer from './util/GraphPlayer';
 import Table from './Table';
+import Voronoi from 'Voronoi';
 
 /**
  * Fading flash duration when a node/edge is accessed (in ms)
  */
 const FADE_DURATION = 250;
+
+let dragged = null;
 
 /**
  *
@@ -42,33 +45,102 @@ export default function RenderedGraph({ graph }) {
 
       // --- Drawing vertex highlight background
       p5.push();
-      for (let i = 0; i < vertices.length; ++i) {
-        const { position, lastRead, highlights } = vertices[i];
-        for (const color of highlights) {
-          // prettier-ignore
-          p5.fill(
-            255 * (color % 2),
-            255 * ((color>>1) % 2),
-            255 * ((color>>2) % 2),
-            20
-          );
-          p5.noStroke();
-          p5.ellipse(position.x, position.y, 30, 30);
-          p5.strokeWeight(30);
-          // prettier-ignore
-          p5.stroke(
-            255 * (color % 2),
-            255 * ((color>>1) % 2),
-            255 * ((color>>2) % 2),
-            10
-          );
-          for (const { to } of graph.edgeLists[i]) {
-            if (vertices[to].highlights.has(color)) {
-              const { x, y } = vertices[to].position;
-              p5.line(position.x, position.y, x, y);
-            }
+      // for (let i = 0; i < vertices.length; ++i) {
+      //   const { position, lastRead, highlights } = vertices[i];
+      //   for (const color of highlights) {
+      //     // prettier-ignore
+      //     p5.fill(
+      //       255 * (color % 2),
+      //       255 * ((color >> 1) % 2),
+      //       255 * ((color >> 2) % 2),
+      //       20
+      //     );
+      //     p5.noStroke();
+      //     p5.ellipse(position.x, position.y, 30, 30);
+      //     p5.strokeWeight(30);
+      //     // prettier-ignore
+      //     p5.stroke(
+      //       255 * (color % 2),
+      //       255 * ((color >> 1) % 2),
+      //       255 * ((color >> 2) % 2),
+      //       10
+      //     );
+      //     for (const { to } of graph.edgeLists[i]) {
+      //       if (vertices[to].highlights.has(color)) {
+      //         const { x, y } = vertices[to].position;
+      //         p5.line(position.x, position.y, x, y);
+      //       }
+      //     }
+      //   }
+      // }
+      // Use Voronoi on the highlight colors to draw the background, do not draw voronoi edge, color should be the same color as the highlight of the vertext it contains
+      const voronoi = new Voronoi();
+      const bbox = { xl: 0, xr: p5.width, yt: 0, yb: p5.height };
+      const sites = [];
+      for (const vertex of vertices) {
+        // if (vertex.highlights.size === 0) {
+        //   sites.push({
+        //     x: vertex.position.x,
+        //     y: vertex.position.y,
+        //     color: 0,
+        //   });
+        // }
+        for (const color of vertex.highlights) {
+          sites.push({
+            x: vertex.position.x,
+            y: vertex.position.y,
+            color,
+          });
+        }
+      }
+      const diagram = voronoi.compute(sites, bbox);
+
+      // find shared edges between cells
+      const sharedEdges = new Map();
+      for (const cell of diagram.cells) {
+        for (const halfedge of cell.halfedges) {
+          const { x: xstart, y: ystart } = halfedge.getStartpoint();
+          const { x: xend, y: yend } = halfedge.getEndpoint();
+          const key = `${Math.min(xstart, xend)} ${Math.min(ystart, yend)} ${Math.max(xstart, xend)} ${Math.max(ystart, yend)}`;
+          if (sharedEdges.has(key)) {
+            sharedEdges.get(key).push(cell);
+          } else {
+            sharedEdges.set(key, [cell]);
           }
         }
+      }
+
+      for (const cell of diagram.cells) {
+        p5.fill(
+          255 * (cell.site.color % 2),
+          255 * ((cell.site.color >> 1) % 2),
+          255 * ((cell.site.color >> 2) % 2),
+          25,
+        );
+        p5.strokeWeight(0);
+        p5.beginShape();
+        // draw shape with rounded corners by stopping each edge a bit early and using curveVertex() to connect them
+        for (let i = 0; i < cell.halfedges.length; i++) {
+          let { x: xstart, y: ystart } = cell.halfedges[i % cell.halfedges.length].getStartpoint();
+          let { x: xend, y: yend } = cell.halfedges[i % cell.halfedges.length].getEndpoint();
+          let { x: xnextstart, y: ynextstart } = cell.halfedges[(i + 1) % cell.halfedges.length].getStartpoint();
+          let { x: xnextend, y: ynextend } = cell.halfedges[(i + 1) % cell.halfedges.length].getEndpoint();
+
+          // stop a bit early to curve the corner
+          let xsub = (xend - xstart) * 0.2;
+          let ysub = (yend - ystart) * 0.2;
+          let xmid = xend - xsub;
+          let ymid = yend - ysub;
+          let xnextsub = (xnextend - xnextstart) * 0.2;
+          let ynextsub = (ynextend - ynextstart) * 0.2;
+          let xmidnext = xnextstart + xnextsub;
+          let ymidnext = ynextstart + ynextsub;
+
+          // p5.vertex(xmid, ymid);
+          // p5.curveVertex(xmidnext, ymidnext);
+          p5.vertex(xstart, ystart);
+        }
+        p5.endShape(p5.CLOSE);
       }
       p5.pop();
 
@@ -79,8 +151,8 @@ export default function RenderedGraph({ graph }) {
         // prettier-ignore
         p5.stroke(
           255 * (color % 2),
-          255 * ((color>>1) % 2),
-          255 * ((color>>2) % 2),
+          255 * ((color >> 1) % 2),
+          255 * ((color >> 2) % 2),
           100 + 155 * Math.max(0, (FADE_DURATION + lastRead - Date.now()) / FADE_DURATION),
         );
         // prettier-ignore
@@ -102,8 +174,8 @@ export default function RenderedGraph({ graph }) {
         // prettier-ignore
         p5.stroke(
           255 * (color % 2),
-          255 * ((color>>1) % 2),
-          255 * ((color>>2) % 2),
+          255 * ((color >> 1) % 2),
+          255 * ((color >> 2) % 2),
           100 + 155 * Math.max(0, (FADE_DURATION + lastRead - Date.now()) / FADE_DURATION),
         );
         p5.push();
@@ -120,7 +192,7 @@ export default function RenderedGraph({ graph }) {
           p5.push();
           p5.noStroke();
           p5.fill(0);
-          p5.translate(diff.magnitude()/2, 0);
+          p5.translate(diff.magnitude() / 2, 0);
           p5.rotate(-diff.angle());
           p5.text(weight, 0, 0);
           p5.pop();
@@ -142,6 +214,27 @@ export default function RenderedGraph({ graph }) {
           }
         }
       }
+
+      // Make nodes draggable
+      if (p5.mouseIsPressed) {
+        if (dragged === null) {
+          for (let i = 0; i < vertices.length; ++i) {
+            const { position } = vertices[i];
+            const { x, y } = p5.createVector(p5.mouseX, p5.mouseY);
+            if (x > position.x - 15 && x < position.x + 15 && y > position.y - 15 && y < position.y + 15) {
+              dragged = i;
+            }
+          }
+        } else {
+          const { position } = vertices[dragged];
+          position.x = p5.mouseX;
+          position.y = p5.mouseY;
+        }
+      } else {
+        dragged = null;
+      }
+
+
       vertices.forEach((v, i) => v.position.add(impulse[i].multiply(0.5)));
 
       p5.pop();
