daf842c202f6862ca17201fb2bdfd2ecd4029256
GLKumfert <gkumf001@ucr.edu>
2023-12-11 19:10:58 -0800

    Added nicer animations to topo

--- src/algorithm/TopologicalSort.js
+++ src/algorithm/TopologicalSort.js
@@ -1,24 +1,53 @@
-import Graph from '../util/Graph';
+import Graph, { Vertex } from '../util/Graph';
 import BaseAlgorithm from './BaseAlgorithm';
 
 class ToplogicalSort extends BaseAlgorithm {
-  topo(graph, result, table) {
+  /**
+   * @param {Graph} graph
+   * @param {import('../util/Graph').GraphInput} graphInput
+   */
+  topo(graph, result, table, graphInput) {
     let n = graph.vertices.length;
     let counter = 0; // index for result array
     const numbers = Array.from({ length: n }, (_, index) => index);
     // precalculating all incoming edges count
-    for (let i = 0; i < n; i++) {
-      let neigh = graph.getNeighbors(i);
-      // incrementing value when edge is found
-      for (const { to } of neigh) {
-        graph
-          .getVertex(to)
-          .setAuxiliaryValue(0, graph.getVertex(to).getAuxiliaryValue(0) + 1);
+    let incoming = Array.from({ length: n }, () => []);
+    graph.subroutine('precompute incoming edges', () => {
+        // keep track of all incoming edges by pushing them to an array of array of edges
+      for (const [from, to] of graphInput.edges) {
+        incoming[to].push(graph.getEdge(from, to));
       }
+    });
+    for(let i = 0 ; i < n; i++){
+        let u = graph.getVertex(i);
+        u.setColor(2);
+        for(const edge of incoming[i]){
+            edge.setColor(1);
+        }
+        graph.getVertex(i).setAuxiliaryValue(0, incoming[i].length);
+        table.set(0, i, graph.getVertex(i).getAuxiliaryValue(0));
+
+        graph.subroutine('deallocate colors' , () => {
+            for(const edge of incoming[i]){
+                edge.setColor(0);
+            }
+            u.setColor(0);
+        });
     }
-    for (let i = 0; i < n; i++) {
-      table.set(0, i, graph.getVertex(i).getAuxiliaryValue(0)); // setting in table for visuals
-    }
+    // old way of precomputing, changing for animation
+    // for (let i = 0; i < n; i++) {
+    //   let u = graph.getVertex(i);
+    //   let neigh = graph.getNeighbors(i);
+    //   // incrementing value when edge is found
+    //   for (const { to } of neigh) {
+    //     graph
+    //       .getVertex(to)
+    //       .setAuxiliaryValue(0, graph.getVertex(to).getAuxiliaryValue(0) + 1);
+    //   }
+    // }
+    // for (let i = 0; i < n; i++) {
+    //   table.set(0, i, graph.getVertex(i).getAuxiliaryValue(0)); // setting in table for visuals
+    // }
     const vals = numbers.map((number) => [
       number,
       graph.getVertex(number).getAuxiliaryValue(0),
@@ -28,13 +57,22 @@ class ToplogicalSort extends BaseAlgorithm {
     while (filteredTuples.length > 0) {
       // while there are verticies with 0 incoming edges
       for (const [v] of filteredTuples) {
+        let u = graph.getVertex(v);
+        u.setColor(1);
         result.set(0, counter, v); // adding to our topo sort (greedy)
         counter++;
         for (const { to } of graph.getNeighbors(v)) {
+            graph.getEdge(v, to).setColor(1);
           // decrementing counts by incrementing
           vals[to][1] -= 1;
           table.set(0, to, vals[to][1]);
         }
+        graph.subroutine('deallocate colors', () => {
+            for (const { to } of graph.getNeighbors(v)) {
+                graph.getEdge(v, to).setColor(0);
+            }
+        });
+        u.setColor(2);
         vals[v][1] = -1; // to avoid recounting.
       }
       filteredTuples = vals.filter((tuple) => tuple[1] === 0);
@@ -52,7 +90,7 @@ class ToplogicalSort extends BaseAlgorithm {
     graph.finalize();
 
     // --- process graph
-    this.topo(graph, result, table);
+    this.topo(graph, result, table, graphInput);
     return graph;
   }
 }
