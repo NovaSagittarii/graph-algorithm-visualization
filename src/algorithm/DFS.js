eae74521c946e5657653a082d4122321a469cb11
Joshua <joshuahsu108@gmail.com>
2023-12-11 17:27:36 -0800

    Add edge coloring based on edge type

--- src/algorithm/DFS.js
+++ src/algorithm/DFS.js
@@ -2,14 +2,36 @@ import Graph from '../util/Graph';
 import BaseAlgorithm from './BaseAlgorithm';
 
 class DFS extends BaseAlgorithm {
-  dfs(graph, from, hops, arrivaldeparture) {
+  dfs(graph, parent, from, hops, arrivaldeparture) {
     const u = graph.getVertex(from);
-    const visited = u.getAuxiliaryValue(0);
-
-    if (visited) return;
-    u.setAuxiliaryValue(0, true); // mark as visited
+    if (parent === null && arrivaldeparture.get(0, from)[0] !== null) {
+      return;
+    }
+    if (arrivaldeparture.get(0, from)[1] !== null) {
+      if (arrivaldeparture.get(0, from)[1] < this.plane) { // Forward edge
+        const edge = graph.getEdge(parent, from);
+        edge.setColor(2);
+        return;
+      }
+      else { // Cross edge
+        const edge = graph.getEdge(parent, from);
+        edge.setColor(3);
+        return;
+      }
+    }
+    if (arrivaldeparture.get(0, from)[0] !== null) {
+      if (arrivaldeparture.get(0, from)[0] < this.plane) { // Back edge
+        const edge = graph.getEdge(parent, from);
+        edge.setColor(3);
+        return;
+      }
+    }
     u.setColor(1);
     u.addHighlight(hops + 1);
+    if (parent !== null) {
+      const edge = graph.getEdge(parent, from);
+      edge.setColor(1);
+    }
 
     console.log(this.plane);
     arrivaldeparture.set(0, from, [this.plane, null]);
@@ -23,8 +45,7 @@ class DFS extends BaseAlgorithm {
     for (const { to } of neighbors) {
       // add to next frontier if not visited yet
       const v = graph.getVertex(to);
-      if (v.getAuxiliaryValue(0)) continue;
-      this.dfs(graph, to, hops + 1, arrivaldeparture);
+      this.dfs(graph, from, to, hops + 1, arrivaldeparture);
     }
 
     console.log(this.plane);
@@ -42,7 +63,7 @@ class DFS extends BaseAlgorithm {
 
     // --- process graph
     for (let from = 0; from < graphInput.n; ++from) {
-      this.dfs(graph, from, 0, arrivaldeparture);
+      this.dfs(graph, null, from, 0, arrivaldeparture);
     }
     return graph;
   }
