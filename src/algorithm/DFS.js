1fbea27126ed9ebf83f243d33cf47774e3dd8447
Joshua <joshuahsu108@gmail.com>
2023-12-11 17:05:35 -0800

    Add pre post to dfs

--- src/algorithm/DFS.js
+++ src/algorithm/DFS.js
@@ -2,11 +2,13 @@ import Graph from '../util/Graph';
 import BaseAlgorithm from './BaseAlgorithm';
 
 class DFS extends BaseAlgorithm {
-  dfs(graph, from, hops, visits) {
+  dfs(graph, from, hops, arrivaldeparture, plane) {
     const u = graph.getVertex(from);
     const visited = u.getAuxiliaryValue(0);
 
-    visits.set(0, from, visits.get(0, from) + 1); // increment a table element
+    arrivaldeparture.set(0, from, [plane, null]);
+    let arr = plane;
+    plane += 1;
 
     if (visited) return;
     u.setAuxiliaryValue(0, true); // mark as visited
@@ -22,19 +24,23 @@ class DFS extends BaseAlgorithm {
       // add to next frontier if not visited yet
       const v = graph.getVertex(to);
       if (v.getAuxiliaryValue(0)) continue;
-      this.dfs(graph, to, hops + 1, visits);
+      plane = this.dfs(graph, to, hops + 1, arrivaldeparture, plane);
     }
+    arrivaldeparture.set(0, from, [arr, plane]);
+    plane += 1;
+    return plane;
   }
 
   /** @override */
   run(graphInput) {
     // --- set up auxiliary values then finalize
     const graph = new Graph(graphInput, [false], null);
-    const visits = graph.createTable(1, graphInput.n, 0);
+    const arrivaldeparture = graph.createTable(1, graphInput.n, [null, null], (x) => x[0] + ',' + x[1]);
+    let plane = 0;
     graph.finalize();
 
     // --- process graph
-    this.dfs(graph, 0, 0, visits);
+    this.dfs(graph, 0, 0, arrivaldeparture, plane);
     return graph;
   }
 }
