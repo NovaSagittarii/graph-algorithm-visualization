96a2f977b0e38f79cf6d3b60efbd901f89c89f76
Joshua <joshuahsu108@gmail.com>
2023-12-12 02:07:29 -0800

    Fix BFS for undirected

--- src/algorithm/DFS.js
+++ src/algorithm/DFS.js
@@ -3,6 +3,13 @@ import BaseAlgorithm from './BaseAlgorithm';
 
 class DFS extends BaseAlgorithm {
   dfs(graph, parent, from, hops, arrivaldeparture) {
+    const edge = graph.getEdge(parent, from);
+    const edge2 = graph.getEdge(from, parent);
+    console.log(edge && edge.getAuxiliaryValue(0), edge2 && edge2.getAuxiliaryValue(0));
+    if ((edge && edge.getAuxiliaryValue(0)) || (edge2 && edge2.getAuxiliaryValue(0))) return;
+    if (edge) edge.setAuxiliaryValue(0, true);
+    if (edge2) edge2.setAuxiliaryValue(0, true);
+
     const u = graph.getVertex(from);
     if (parent === null && arrivaldeparture.get(0, from)[0] !== null) {
       return;
@@ -27,7 +34,7 @@ class DFS extends BaseAlgorithm {
       }
     }
     u.setColor(1);
-    u.addHighlight(hops + 1);
+    u.addHighlight(2);
     if (parent !== null) {
       const edge = graph.getEdge(parent, from);
       edge.setColor(1);
@@ -56,7 +63,7 @@ class DFS extends BaseAlgorithm {
   /** @override */
   run(graphInput) {
     // --- set up auxiliary values then finalize
-    const graph = new Graph(graphInput, [false], null);
+    const graph = new Graph(graphInput, [false], [false]);
     const arrivaldeparture = graph.createTable({
       name: "Arrival and Departure",
       rows: 1,
@@ -66,6 +73,17 @@ class DFS extends BaseAlgorithm {
     });
     this.plane = 0;
     graph.finalize();
+    // add auxillary visited to edge
+    graph.subroutine('initialize auxiliary values', () => {
+      for (let from = 0; from < graphInput.n; ++from) {
+        for (const { to } of graph.getNeighbors(from)) {
+          const edge = graph.getEdge(from, to);
+          if (edge) edge.setAuxiliaryValue(0, false);
+          const edge2 = graph.getEdge(to, from);
+          if (edge2) edge2.setAuxiliaryValue(0, false);
+        }
+      }
+    });
 
     // --- process graph
     for (let from = 0; from < graphInput.n; ++from) {
